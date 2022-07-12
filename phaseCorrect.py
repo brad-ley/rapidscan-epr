6645794fe3ed68cce7207d9ee74460a9de05054f
Brad P <bdprice@ucsb.edu>
2022-07-11 23:52:01 -0700

    new upload

--- phaseCorrect.py
+++ phaseCorrect.py
@@ -12,26 +12,45 @@ from dash import dcc, html
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 from dash.long_callback import DiskcacheLongCallbackManager
+from joblib import Memory, Parallel, delayed
 from plotly import graph_objects as go
 from plotly.offline import iplot
 from scipy.optimize import curve_fit as cf
-from joblib import Parallel, delayed
 
 from cycler import cycle
 
 cache = diskcache.Cache("./cache")
 long_callback_manager = DiskcacheLongCallbackManager(cache)
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+memory = Memory('/Users/Brad/Documents/Research/code/python/tigger/cache',
+                verbose=0)
 
 app = dash.Dash(__name__,
                 external_stylesheets=external_stylesheets,
                 prevent_initial_callbacks=False,
                 long_callback_manager=long_callback_manager)
 app.title = 'Phase correction'
+app._favicon = 'assets/favicon.ico'
+theme = {
+    'dark': True,
+    'detail': '#007439',
+    'primary': '#00EA64',
+    'secondary': '#6E6E6E',
+}
 
 graph_height = '325px'
 margin_vert = '0px'
 margin = dict(l=20, r=20, t=20, b=20)
+
+five_cycles = 5 / (70E6)  # s
+
+def is_int(val):
+    try:
+        int(val)
+
+        return True
+    except ValueError:
+        return False
 
 
 def make_fig():
@@ -68,7 +87,7 @@ def parse_contents(n, filepath, d):
                             skiprows=r + 1,
                             sep=', ',
                             on_bad_lines='skip',
-                            engine='python')
+                            engine='python',)
 
             def sin(x, a, b, c, d):
                 return a + b * np.sin(c * x + d)
@@ -96,8 +115,8 @@ def parse_contents(n, filepath, d):
 
             return html.Div(f"Loaded {P(filepath).name}",
                             style={'color': 'green'
-                                   }), fig, d.to_json(date_format='iso',
-                                                      orient='split'), r + 1
+                                   }), fig, d.to_json(date_format='iso', orient='split'
+                                                      ), r + 1
         elif filepath in ['/', '']:
             h = html.Div(['Choose file above'])
         elif P(filepath).is_file():
@@ -117,8 +136,27 @@ def parse_contents(n, filepath, d):
     return h, fig, d, 0
 
 
+@app.callback(Output('dummy', 'children'), Input('save', 'n_clicks'),
+              State('average', 'data'), State('filepath', 'value'))
+def save(_, avg, filepath):
+    try:
+        avg = pd.read_json(avg, orient='split')
+        avg.to_csv(
+            P(filepath).parent.joinpath(
+            "avg_" + "_".join(P(filepath).stem.split("_")[:-1]) +
+            P(filepath).suffix)
+                )
+    except ValueError:
+        pass
+
+    return ""
+
+
 @app.long_callback(Output('phased', 'figure'),
+                   Output('Qs', 'figure'),
+                   Output('average', 'data'),
                    Input('phase', 'n_clicks'),
+                   Input('Q-factor', 'value'),
                    State('alldata', 'data'),
                    State('ref-file', 'data'),
                    running=[
@@ -131,44 +169,86 @@ def parse_contents(n, filepath, d):
                        }),
                    ],
                    prevent_initial_call=True)
-def phase(_, alldata, ref_file):
-    alld = pd.read_json(alldata, orient='split')
-    # holdd = pd.DataFrame(columns=['x', *list(range(1,len(alld.columns)-1))])
-    holdd = {}
-    temp = {}
-    refdat = pd.read_json(ref_file, orient='split')
-    ref_signal = refdat[refdat.columns[1]] + 1j * refdat[refdat.columns[2]]
+def phase(_, qfact, alldata, ref_file):
+    try:
+        alld = pd.read_json(alldata, orient='split')
+        # holdd = pd.DataFrame(columns=['time', *list(range(1,len(alld.columns)-1))])
+        holdd = {}
+        temp = {}
+        refdat = pd.read_json(ref_file, orient='split')
+        ref_signal = refdat[refdat.columns[1]] + 1j * refdat[refdat.columns[2]]
 
-    cols = [ii for ii in alld.columns if ii != 'x']
-    holdd['x'] = alld['x']
-    temp['x'] = alld['x']
-    numpyref = ref_signal.to_numpy()
-    stop = np.where(temp['x'].to_numpy() < 50e-6)
+        temp['time'] = alld['time']
+        start = 0
+        stop = np.where(temp['time'].to_numpy() < five_cycles)[0][-1]
 
-    def loop(col):
-        try:
-            sig = np.array(
-                [ii['real'] + 1j * ii['imag'] for ii in list(alld[col])])
-            phi = np.angle(
-                np.dot(np.conjugate(sig)[0:stop], numpyref[0:stop]))
-            sig *= np.exp(1j * phi)
-            holdd[col] = np.real(sig)
-            temp[col] = sig
-        except TypeError:  # sometimes there is a nan column
-            pass
+        cols = [ii for ii in alld.columns if ii != 'time']
+        holdd['time'] = alld['time'][start:stop]
+        numpyref = ref_signal.to_numpy()
+        qfact = float(qfact)
+        Qs = []
+        XQs = []
+        # @memory.cache
+        # def loop(col):
 
-    Parallel(n_jobs=4)(delayed(loop)(col) for col in cols) 
+        reft = numpyref[start:stop]
 
-    alld = pd.DataFrame(temp)
-    holdd = pd.DataFrame(holdd)
-    holdd['ref'] = refdat[refdat.columns[1]]
-    fig = px.line(holdd,
-                  x='x',
-                  y=holdd.columns[1::max(len(holdd.columns) // 5, 1)])
+        for col in cols:
+            try:
+                sig = np.array(
+                    [ii['real'] + 1j * ii['imag'] for ii in list(alld[col])])
+                sigt = sig[start:stop]
+                phi = np.angle(np.dot(np.conjugate(sigt), reft))
+                sig *= np.exp(1j * phi)
+                sigo = sig[start:stop]
+                Q = np.sum(np.abs(sigo - reft)) / np.sum(np.abs(reft)) * 100
+
+                if Q < qfact:
+                    Qs.append(Q)
+                    temp[col] = sig
+                    holdd[col] = np.real(sigo)
+                else:
+                    XQs.append(Q)
+            # return holdd, temp
+            except TypeError:  # sometimes there is a nan column
+                # except FileExistsError:
+                pass
+
+        # holdd, temp = Parallel(n_jobs=4)(delayed(loop)(col) for col in cols)
+
+        alld = pd.DataFrame(temp)
+        holdd = pd.DataFrame(holdd)
+        holdd['avg'] = holdd.mean(axis=1)
+        holdd['ref'] = refdat[refdat.columns[1]]
+        plots = [ii for ii in list(holdd.columns) if is_int(ii)]
+        plots = plots[::max(len(plots) // 5, 1)] + ['avg', 'ref']
+
+        fig = px.line(holdd, x='time', y=plots)
+        # fig.update_xaxes(range=[min(holdd['time']), min(holdd['time']) + 1e-6])
+
+        s1 = pd.Series(Qs, name='Qs', dtype='object')
+        s2 = pd.Series(XQs, name='XQs', dtype='object')
+        quals = pd.concat([s1, s2], axis=1)
+        qfig = px.histogram(quals, nbins=20)
+
+    # except ValueError:
+    except FileExistsError:
+        fig = px.line()
+        qfig = px.histogram()
+        alld = pd.DataFrame()
+
+    savedat = {
+        'time': alld['time'],
+        'avg': alld.loc[:, alld.columns != 'time'].mean(axis=1)
+    }
     fig.update_layout(margin=margin)
-    fig.update_xaxes(range=[min(holdd['x']), min(holdd['x']) + 1e-6])
+    qfig.update_layout(margin=margin)
+    qfig.update_xaxes(title_text='phase quality Q')
+    # qfig.update_xaxes(range=[0, 10])
 
-    return fig
+    return fig, qfig, pd.DataFrame(savedat).to_json(date_format='iso', orient='split'
+                                                    )
+    # return fig, qfig, #savedat
 
 
 @app.long_callback(
@@ -203,29 +283,34 @@ def process(set_progress, _, filepath, ref_file, skiprows):
             if "_".join(P(filepath).stem.split("_")[:-1]) in str(ii)
             and ii.suffix == P(filepath).suffix
         ]
-        # files = files[:2]
-        # alld = pd.DataFrame(columns=['x', *list(range(1,len(files)))])
-        # holdd = pd.DataFrame(columns=['x', *list(range(1,len(files)))])
+        files = files[:300]
+        # alld = pd.DataFrame(columns=['time', *list(range(1,len(files)))])
+        # holdd = pd.DataFrame(columns=['time', *list(range(1,len(files)))])
         alld = {}
         holdd = {}
         ref_d = pd.read_json(ref_file, orient='split')
-        alld['x'] = ref_d[ref_d.columns[0]]
-        holdd['x'] = ref_d[ref_d.columns[0]]
+        alld['time'] = ref_d[ref_d.columns[0]]
+
+        start = 0
+        stop = np.where(alld['time'].to_numpy() < five_cycles)[0][-1]
+
+        holdd['time'] = ref_d[ref_d.columns[0]][start:stop]
 
         for i, f in enumerate(files):
             d = pd.read_csv(f,
                             skiprows=skiprows,
                             sep=', ',
                             on_bad_lines='skip',
-                            engine='python')
+                            engine='python', 
+                            )
             alld[i] = d[d.columns[1]] + 1j * d[d.columns[2]]
-            holdd[i] = d[d.columns[1]]
+            holdd[i] = d[d.columns[1]][start:stop]
             set_progress((str(i + 1), str(len(files))))
 
         alld = pd.DataFrame(alld)
         holdd = pd.DataFrame(holdd)
         fig = px.line(holdd,
-                      x='x',
+                      x='time',
                       y=holdd.columns[1::max(len(holdd.columns) // 5, 1)])
         fig.update_layout(margin=margin)
     # except IndexError:
@@ -258,13 +343,23 @@ app.layout = html.Div(
                 'width': '49%',
                 'horizontal-align': 'middle'
             }),
-        html.Div([
-            dcc.Graph(id='phased', figure=fig, style={'height': graph_height})
-        ],
-                 style={
-                     'width': '98%',
-                     'align': 'center'
-                 }),
+        html.Div(
+            [
+                dcc.Graph(
+                    id='phased', figure=fig, style={'height': graph_height})
+            ],
+            style={
+                'display': 'inline-block',
+                'width': '49%',
+                'horizontal-align': 'middle'
+            }),
+        html.Div(
+            [dcc.Graph(id='Qs', figure=fig, style={'height': graph_height})],
+            style={
+                'display': 'inline-block',
+                'width': '49%',
+                'horizontal-align': 'middle'
+            }),
         html.Div([
             html.Button(id='load',
                         n_clicks=0,
@@ -294,11 +389,28 @@ app.layout = html.Div(
                     'visibility': 'hidden',
                     'margin': '0px 20px 0px 20px'
                 }),
-            "Max Q: ",
-            dcc.Input(
-                id='Q-factor',
-                value='100',
-                type='number',
+            html.Div([
+                "Max Q:",
+            ], style={'display': 'inline-block'}),
+            html.Div(
+                [
+                    dcc.Slider(
+                        0,
+                        100,
+                        id='Q-factor',
+                        value=10,
+                        marks=None,
+                        tooltip={
+                            "placement": "right",
+                            "always_visible": True
+                        },
+                    )
+                ],
+                style={
+                    'width': '32%',
+                    'display': 'inline-block',
+                    'margin': '0px 0px -25px 0px'
+                },
             ),
         ]),
         html.Div(
@@ -318,12 +430,23 @@ app.layout = html.Div(
                         'textAlign': 'left',
                         'margin': '0px 0px 0px 0%'
                     }),
+                html.Button(id='save',
+                            n_clicks=0,
+                            children='Save average',
+                            style={
+                                'background-color': 'lightblue',
+                                'margin': '0px 0px 0px 2%',
+                                'height': '75px',
+                                'width': '150px'
+                            }),
                 html.Div(id='fileout', children=''),
+                html.Div(id='dummy', children='')
             ],
             style={'margin': '10px 0px 0px 2%'}),
         dcc.Store(id='ref-file'),
         dcc.Store(id='skiprows'),
         dcc.Store(id='alldata'),
+        dcc.Store(id='average'),
         html.Div(id='progress'),
         # dcc.Interval(id='interval', interval=500)
     ],
