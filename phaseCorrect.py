b575425ef1627a2635cecd862f2abf0d98a0f734
Brad P <bdprice@ucsb.edu>
2022-07-13 10:46:06 -0700

    fixing hanging with dash.no_update and PreventUpdate

--- phaseCorrect.py
+++ phaseCorrect.py
@@ -155,6 +155,7 @@ def save(_, avg, filepath):
 @app.long_callback(Output('phased', 'figure'),
                    Output('Qs', 'figure'),
                    Output('average', 'data'),
+                   Output('phase', 'n_clicks'),
                    Input('phase', 'n_clicks'),
                    Input('Q-factor', 'value'),
                    State('alldata', 'data'),
@@ -169,70 +170,77 @@ def save(_, avg, filepath):
                        }),
                    ],
                    prevent_initial_call=True)
-def phase(_, qfact, alldata, ref_file):
+def phase(n_clicks, qfact, alldata, ref_file):
     try:
-        alld = pd.read_json(alldata, orient='split')
-        # holdd = pd.DataFrame(columns=['time', *list(range(1,len(alld.columns)-1))])
-        holdd = {}
-        temp = {}
-        refdat = pd.read_json(ref_file, orient='split')
-        ref_signal = refdat[refdat.columns[1]] + 1j * refdat[refdat.columns[2]]
+        if n_clicks is not None and n_clicks > 0:
+            alld = pd.read_json(alldata, orient='split')
+            # holdd = pd.DataFrame(columns=['time', *list(range(1,len(alld.columns)-1))])
+            holdd = {}
+            temp = {}
+            refdat = pd.read_json(ref_file, orient='split')
+            ref_signal = refdat[refdat.columns[1]] + 1j * refdat[refdat.columns[2]]
 
-        temp['time'] = alld['time']
-        start = 0
-        stop = np.where(temp['time'].to_numpy() < five_cycles)[0][-1]
+            temp['time'] = alld['time']
+            start = 0
+            stop = np.where(temp['time'].to_numpy() < five_cycles)[0][-1]
 
-        cols = [ii for ii in alld.columns if ii != 'time']
-        holdd['time'] = alld['time'][start:stop]
-        numpyref = ref_signal.to_numpy()
-        qfact = float(qfact)
-        Qs = []
-        XQs = []
-        # @memory.cache
-        # def loop(col):
+            cols = [ii for ii in alld.columns if ii != 'time']
+            holdd['time'] = alld['time'][start:stop]
+            numpyref = ref_signal.to_numpy()
+            qfact = float(qfact)
+            Qs = []
+            XQs = []
+            # @memory.cache
+            # def loop(col):
 
-        reft = numpyref[start:stop]
+            reft = numpyref[start:stop]
 
-        for col in cols:
-            try:
-                sig = np.array(
-                    [ii['real'] + 1j * ii['imag'] for ii in list(alld[col])])
-                sigt = sig[start:stop]
-                phi = np.angle(np.dot(np.conjugate(sigt), reft))
-                sig *= np.exp(1j * phi)
-                sigo = sig[start:stop]
-                Q = np.sum(np.abs(sigo - reft)) / np.sum(np.abs(reft)) * 100
+            for col in cols:
+                try:
+                    sig = np.array(
+                        [ii['real'] + 1j * ii['imag'] for ii in list(alld[col])])
+                    sigt = sig[start:stop]
+                    phi = np.angle(np.dot(np.conjugate(sigt), reft))
+                    sig *= np.exp(1j * phi)
+                    sigo = sig[start:stop]
+                    Q = np.sum(np.abs(sigo - reft)) / np.sum(np.abs(reft)) * 100
 
-                if Q < qfact:
-                    Qs.append(Q)
-                    temp[col] = sig
-                    holdd[col] = np.real(sigo)
-                else:
-                    XQs.append(Q)
-            # return holdd, temp
-            except TypeError:  # sometimes there is a nan column
-                # except FileExistsError:
-                pass
+                    if Q < qfact:
+                        Qs.append(Q)
+                        temp[col] = sig
+                        holdd[col] = np.real(sigo)
+                    else:
+                        XQs.append(Q)
+                # return holdd, temp
+                except TypeError:  # sometimes there is a nan column
+                    # except FileExistsError:
+                    pass
 
-        # holdd, temp = Parallel(n_jobs=4)(delayed(loop)(col) for col in cols)
+            # holdd, temp = Parallel(n_jobs=4)(delayed(loop)(col) for col in cols)
 
-        alld = pd.DataFrame(temp)
-        holdd = pd.DataFrame(holdd)
-        holdd['avg'] = holdd.mean(axis=1)
-        holdd['ref'] = refdat[refdat.columns[1]]
-        plots = [ii for ii in list(holdd.columns) if is_int(ii)]
-        plots = plots[::max(len(plots) // 5, 1)] + ['avg', 'ref']
+            alld = pd.DataFrame(temp)
+            holdd = pd.DataFrame(holdd)
+            holdd['avg'] = holdd.mean(axis=1)
+            holdd['ref'] = refdat[refdat.columns[1]]
+            plots = [ii for ii in list(holdd.columns) if is_int(ii)]
+            plots = plots[::max(len(plots) // 5, 1)] + ['avg', 'ref']
 
-        fig = px.line(holdd, x='time', y=plots)
-        # fig.update_xaxes(range=[min(holdd['time']), min(holdd['time']) + 1e-6])
+            fig = px.line(holdd, x='time', y=plots)
+            # fig.update_xaxes(range=[min(holdd['time']), min(holdd['time']) + 1e-6])
 
-        s1 = pd.Series(Qs, name='Qs', dtype='object')
-        s2 = pd.Series(XQs, name='XQs', dtype='object')
-        quals = pd.concat([s1, s2], axis=1)
-        qfig = px.histogram(quals, nbins=20)
+            s1 = pd.Series(Qs, name='Qs', dtype='object')
+            s2 = pd.Series(XQs, name='XQs', dtype='object')
+            quals = pd.concat([s1, s2], axis=1)
+            qfig = px.histogram(quals, nbins=20)
 
-    # except ValueError:
-    except FileExistsError:
+        # except ValueError:
+        else:
+            raise PreventUpdate
+        # qfig.update_xaxes(range=[0, 10])
+    except PreventUpdate:
+            return dash.no_update, dash.no_update, dash.no_update, 0
+
+    except (FileExistsError):
         fig = px.line()
         qfig = px.histogram()
         alld = pd.DataFrame()
@@ -244,16 +252,16 @@ def phase(_, qfact, alldata, ref_file):
     fig.update_layout(margin=margin)
     qfig.update_layout(margin=margin)
     qfig.update_xaxes(title_text='phase quality Q')
-    # qfig.update_xaxes(range=[0, 10])
 
     return fig, qfig, pd.DataFrame(savedat).to_json(date_format='iso', orient='split'
-                                                    )
+                                                    ), 0
     # return fig, qfig, #savedat
 
 
 @app.long_callback(
     Output('allplots', 'figure'),
     Output('alldata', 'data'),
+    Output('process', 'n_clicks'),
     Input('process', 'n_clicks'),
     # Input('filepath', 'value'),
     State('filepath', 'value'),
@@ -276,50 +284,54 @@ def phase(_, qfact, alldata, ref_file):
     progress=[Output("progress_bar", "value"),
               Output("progress_bar", "max")],
     prevent_initial_call=True)
-def process(set_progress, _, filepath, ref_file, skiprows):
+def process(set_progress, n_clicks, filepath, ref_file, skiprows):
     try:
-        files = [
-            ii for ii in P(filepath).parent.iterdir()
-            if "_".join(P(filepath).stem.split("_")[:-1]) in str(ii)
-            and ii.suffix == P(filepath).suffix
-        ]
-        files = files[:300]
-        # alld = pd.DataFrame(columns=['time', *list(range(1,len(files)))])
-        # holdd = pd.DataFrame(columns=['time', *list(range(1,len(files)))])
-        alld = {}
-        holdd = {}
-        ref_d = pd.read_json(ref_file, orient='split')
-        alld['time'] = ref_d[ref_d.columns[0]]
+        if n_clicks is not None and n_clicks > 0:
+            files = [
+                ii for ii in P(filepath).parent.iterdir()
+                if "_".join(P(filepath).stem.split("_")[:-1]) == "_".join(ii.stem.split("_")[:-1])
+                and ii.suffix == P(filepath).suffix
+            ]
+            # files = files[:300]
+            # alld = pd.DataFrame(columns=['time', *list(range(1,len(files)))])
+            # holdd = pd.DataFrame(columns=['time', *list(range(1,len(files)))])
+            alld = {}
+            holdd = {}
+            ref_d = pd.read_json(ref_file, orient='split')
+            alld['time'] = ref_d[ref_d.columns[0]]
 
-        start = 0
-        stop = np.where(alld['time'].to_numpy() < five_cycles)[0][-1]
+            start = 0
+            stop = np.where(alld['time'].to_numpy() < five_cycles)[0][-1]
 
-        holdd['time'] = ref_d[ref_d.columns[0]][start:stop]
+            holdd['time'] = ref_d[ref_d.columns[0]][start:stop]
 
-        for i, f in enumerate(files):
-            d = pd.read_csv(f,
-                            skiprows=skiprows,
-                            sep=', ',
-                            on_bad_lines='skip',
-                            engine='python', 
-                            )
-            alld[i] = d[d.columns[1]] + 1j * d[d.columns[2]]
-            holdd[i] = d[d.columns[1]][start:stop]
-            set_progress((str(i + 1), str(len(files))))
+            for i, f in enumerate(files):
+                d = pd.read_csv(f,
+                                skiprows=skiprows,
+                                sep=', ',
+                                on_bad_lines='skip',
+                                engine='python', 
+                                )
+                alld[i] = d[d.columns[1]] + 1j * d[d.columns[2]]
+                holdd[i] = d[d.columns[1]][start:stop]
+                set_progress((str(i + 1), str(len(files))))
 
-        alld = pd.DataFrame(alld)
-        holdd = pd.DataFrame(holdd)
-        fig = px.line(holdd,
-                      x='time',
-                      y=holdd.columns[1::max(len(holdd.columns) // 5, 1)])
-        fig.update_layout(margin=margin)
-    # except IndexError:
-    except FileExistsError:
+            alld = pd.DataFrame(alld)
+            holdd = pd.DataFrame(holdd)
+            fig = px.line(holdd,
+                          x='time',
+                          y=holdd.columns[1::max(len(holdd.columns) // 5, 1)])
+            fig.update_layout(margin=margin)
+        # except IndexError:
+        else:    
+            raise PreventUpdate
+    except (FileExistsError, ValueError):
         fig = make_fig()
         alld = pd.DataFrame()
+    except PreventUpdate:
+        return dash.no_update, dash.no_update, 0
 
-    return fig, alld.to_json(date_format='iso', orient='split')
-
+    return fig, alld.to_json(date_format='iso', orient='split'), 0
 
 app.layout = html.Div(
     [
