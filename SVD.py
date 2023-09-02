f6b43446474555a4f76c846c5e4e8d9fc0b190fc
Brad P <bdprice@ucsb.edu>
2023-09-01 18:17:04 -0700

    centering SVD works ncier

--- SVD.py
+++ SVD.py
@@ -29,12 +29,13 @@ if __name__ == "__main__":
 fig, ax = plt.subplots()
 
 def decompose(mat, times):
-    mean = np.mean(mat)
 
     mT = np.copy(mat)
     mu = mT.mean(axis=1, keepdims=True)
     mT -= mu
     U, E, V = np.linalg.svd(np.transpose(mT))
+
+    # ff.savefig('/Users/Brad/Desktop/princpal_comps_297.5.png', dpi=600)
 
     k = 2
 
@@ -45,41 +46,66 @@ def decompose(mat, times):
 
     for ii in range(k):
         val = PCAmT[ii, :]
-        # ax.plot(times, val - ii, label=f'$C_{ii+1}$')
-        ax.plot(val / val[np.argmax(np.abs(val))], label=f'$C_{ii+1}$')
+        ax.plot(times, val, label=f'$C_{ii+1}$')
+        # ax.plot(val / val[np.argmax(np.abs(val))], label=f'$C_{ii+1}$')
         # ax.plot(times, val, label=f'$C_{ii+1}$')
         # ax.plot(times, val / np.max(val), label=f'$C_{ii+1}$')
         tot += val
     
     # ax.plot(tot+1)
-    return PCAmT
+    # fu, au = plt.subplots()
+    # au.imshow(V[:, :2] @ V[:, :2].T @ mT + mu, aspect='auto')
+    # fh, ah = plt.subplots()
+    # o = 1
+    # ah.plot((V[:, :o] @ V[:, :o].T @ mT + mu)[:, 0], label='0 ' + str(o))
+    # ah.plot((V[:, :o] @ V[:, :o].T @ mT + mu)[:, 100], label='100 ' + str(o))
+    # ah.plot((V[:, :o] @ V[:, :o].T @ mT + mu)[:, 200], label='200 ' + str(o))
+    # ah.plot((V[:, :o] @ V[:, :o].T @ mT + mu)[:, 300], label='300 ' + str(o))
+    # o = 2
+    # ah.plot((V[:, :o] @ V[:, :o].T @ mT + mu)[:, 0], label='0 ' + str(o))
+    # ah.plot((V[:, :o] @ V[:, :o].T @ mT + mu)[:, 100], label='100 ' + str(o))
+    # ah.plot((V[:, :o] @ V[:, :o].T @ mT + mu)[:, 200], label='200 ' + str(o))
+    # ah.plot((V[:, :o] @ V[:, :o].T @ mT + mu)[:, 300], label='300 ' + str(o))
+    # ah.legend()
+    return PCAmT, V
 
 
 def main(filename):
     mat = feather.read_feather(filename)
     cols = [ii for ii in mat.columns if 'abs' in ii]
     plotfield = 25
-    plotcenter = 1
+    # plotcenter = -15
+    B = mat['B'].to_numpy()
+    first = mat[cols[0]].to_numpy()[np.abs(B) < plotfield]
+    plotcenter = B[np.where(np.abs(B) < plotfield)[0][0] + np.argmax(first)]
+    # plotcenter = B[np.argmax(first[np.abs(B) < plotfield])]
+    # print(plotcenter)
     plotlim = plotcenter + np.array([-plotfield, plotfield])
-    B = mat['B']
     lims = np.where(np.logical_and(B >= plotlim[0], B < plotlim[1]))
     l = lims[0][0]
     h = lims[0][-1]
     B = B[l:h] - plotcenter
 
-    # dat = mat[cols].to_numpy()[l:h, :]
+    dat = mat[cols].to_numpy()[l:h, :]
     # plt.plot(B, dat[:, 7])
     # plt.show()
 
+    ### CENTERING ### 
+    dat = np.zeros(((h-l), len(cols)))
     for ind, col in enumerate(cols):
-        coldat = mat[col][l:h]
-        if ind == 0:
-            dat = np.zeros(((h-l)//2, len(cols)))
+        # center = np.argmax(mat[col][l:h].to_numpy()) + l
+        tdat = mat[col][l:h].to_numpy()
+        n = 2**7
+        rolling = np.array([np.mean(tdat[ii-n:ii+n]) if (ii > n and len(tdat) - ii > n) else 0 for ii, _ in enumerate(tdat)])
+        center = np.argmax(rolling) + l
+        coldat = mat[col].to_numpy()
         try:
-            # dat[:, ind] = coldat.to_numpy()[np.argmax(coldat) - (h-l)//4: np.argmax(coldat) + (h-l)//4]
-            dat[:, ind] = coldat.to_numpy()[len(coldat)//2 - (h-l)//4: len(coldat)//2 + (h-l)//4]
+            dat[:, ind] = coldat[center - int((h-l)/2):center + int((h-l)/2)]
+            # dat[:, ind] = coldat.to_numpy()[len(coldat)//2 - (h-l)//4: len(coldat)//2 + (h-l)//4]
         except ValueError:
-            dat[:, ind] = coldat.to_numpy()[len(coldat)//2 - (h-l)//4: len(coldat)//2 + (h-l)//4 + 1]
+            dat[:, ind] = coldat[center - int((h-l)/2):center + int((h-l)/2) + 1]
+            # dat[:, ind] = coldat.to_numpy()[len(coldat)//2 - (h-l)//4: len(coldat)//2 + (h-l)//4 + 1]
+    ### CENTERING ### 
 
     f, a = plt.subplots()
     a.imshow(dat, aspect='auto', interpolation='none')
@@ -90,15 +116,24 @@ def main(filename):
     f.savefig(P(filename).parent.joinpath('heatmap.png'), dpi=600)
     times = np.array(
             ast.literal_eval(P(filename).parent.joinpath('times.txt').read_text()))
-    PCA = decompose(dat.T, times)
+
+    PCA, V = decompose(dat, times)
+    fh, ah = plt.subplots()
+    ah.plot(B, V[:, 0], label='$C_1$')
+    ah.plot(B, V[:, 1], label='$C_2$')
+    ah.set_xlabel('Field (G)')
+    ah.set_ylabel('Amplitude')
+    ah.legend()
+    fh.savefig(P(filename).parent.joinpath('components.png'), dpi=600)
     # PCA = decompose(dat, times)
+    # ax.set_xlabel('Time (s)')
     ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Signal change (au)')
+    ax.set_ylabel('Amplitude')
     ax.legend()
     fig.savefig(P(filename).parent.joinpath('PCA.png'), dpi=600)
 
 
 if __name__ == "__main__":
-    filename = '/Users/Brad/Library/CloudStorage/GoogleDrive-bdprice@ucsb.edu/My Drive/Research/Data/2023/7/12/LiPc/Tonda/QSH/5000/78mA_16.7kHz_filtered_4dBAtt_acq120s_5000avgs_filtered_batchDecon.feather'
+    filename = '/Users/Brad/Library/CloudStorage/GoogleDrive-bdprice@ucsb.edu/My Drive/Research/Data/2023/5/30/FMN sample/stable/279.6/M01_279.6K_unstable_pre30s_on10s_off470s_25000avgs_filtered_batchDecon.feather'
     main(filename)
     plt.show()
--- fitAbsorptionsParallel.py
+++ fitAbsorptionsParallel.py
@@ -1,11 +1,15 @@
 import ast
 import os
+import time
 from pathlib import Path as P
 from pathlib import PurePath as PP
+from math import ceil
 from dataclasses import dataclass
 from scipy.optimize import curve_fit, minimize, Bounds, fmin, fmin_tnc
 from scipy.signal import windows
 from tqdm import tqdm
+from functools import partial
+from numba import njit, prange
 from deconvolveRapidscan import lorentzian
 
 import PIL
@@ -15,6 +19,7 @@ import numpy as np
 import pandas as pd
 import pyarrow.feather as feather
 import lmfit
+import pybroom
 
 from matplotlib import rc
 
@@ -68,17 +73,39 @@ def fit_fun(x, simulations, to_fit, r):
     return ((to_fit - ret(x, simulations, r))  ** 2) * windows.general_hamming(len(to_fit), 0.75)
 
 
-def proc(queue, ind, adjusted_spectra, dists, r, params, func=fit_fun):
-    spectrum = adjusted_spectra[ind, :]
+def proc(spectrum, dists, r, params, pbar=None, func=fit_fun):
+    # spectrum = adjusted_spectra[ind, :]
     obj = lmfit.Minimizer(fit_fun, params, fcn_args=(dists, spectrum, r))
     res = obj.minimize(method='ampgo')
-    queue.put( (ind, res) )
+    # queue.put( (ind, res) )
+
+    if pbar:
+        pbar.update(1)
     # meth = 'bfgs'
     # res2 = obj.minimize(method=meth)
     # lmfit.report_fit(res2)
 
     # ax.plot(specB, spectrum, label=f'data {ind}')
 
+    return res
+
+
+@njit(parallel=True)
+def jitproc(ind, adjusted_spectra, dists, r, params, pbar=None, func=fit_fun):
+    spectrum = adjusted_spectra[ind, :]
+    obj = lmfit.Minimizer(fit_fun, params, fcn_args=(dists, spectrum, r))
+    res = obj.minimize(method='ampgo')
+    # queue.put( (ind, res) )
+
+    if pbar:
+        pbar.update(1)
+    # meth = 'bfgs'
+    # res2 = obj.minimize(method=meth)
+    # lmfit.report_fit(res2)
+
+    # ax.plot(specB, spectrum, label=f'data {ind}')
+
+    return res
 
 def main(filename):
     mat = feather.read_feather(filename)
@@ -112,14 +139,14 @@ def main(filename):
         adjusted_spectra[ind, :][adjusted_spectra[ind, :] < 0] = 0
 
 
-    _data = np.loadtxt(P('/Users/Brad/Library/CloudStorage/GoogleDrive-bdprice@ucsb.edu/My Drive/Research/Code/dipolar averaging/results64_room_T_0.75_-7.72.txt'), delimiter=',')
+    _data = np.loadtxt(P('/Users/Brad/Library/CloudStorage/GoogleDrive-bdprice@ucsb.edu/My Drive/Research/Code/dipolar averaging/results_room_T_0.75_-7.72.txt'), delimiter=',')
     # fig, ax = plt.subplots(figsize=(6,4), layout='constrained')
     B = _data[0, :] * 10 # G
     B -= B[np.argmax(_data[-1, :])] # center to peak, use narrowest peak because weird stuff happens at broadest
     dists = _data[1:, :]
 
     interp_dists = np.zeros((len(_data[1:, 0]), len(specB)))
-    r = np.linspace(2, 6, len(_data[1:, 0]))
+    r = np.linspace(1.8, 6, len(_data[1:, 0]))
 
     for ind, dist in enumerate(_data[1:, :]):
         # dist /= np.max(_data)
@@ -129,6 +156,7 @@ def main(filename):
     fig, ax = plt.subplots(figsize=(6,4))
     ff, aa = plt.subplots(figsize=(6,4))
     f, a = plt.subplots(figsize=(6,4))
+    trans = a.get_xaxis_transform()  # x in data untis, y in axes fraction
     # bounds = Bounds([2, 0.05, -np.max(adjusted_spectra[-1, :]), np.min(adjusted_spectra[-1, :])],\
     #         [6, 2, np.max(adjusted_spectra[-1, :]), np.max(adjusted_spectra[-1, :])])
 
@@ -136,7 +164,11 @@ def main(filename):
     params = lmfit.create_params(
             r0=dict(value=5, vary=True, min=2.1, max=4),
             w0=dict(value=0.075, vary=True, min=0.05, max=0.125),
-            # offset=dict(value=-np.min(interp_dists), min=-np.max(interp_dists), max=np.max(interp_dists)),
+            # # offset=dict(value=-np.min(interp_dists), min=-np.max(interp_dists), max=np.max(interp_dists)),
+            # amp=dict(value=np.max(adjusted_spectra[-1, :]) / np.max(interp_dists[len(_data[1:, 0])//2]), min=0),
+            # offset=dict(value=-np.min(interp_dists)),
+            # r0=dict(value=5, vary=True,),
+            # w0=dict(value=0.075, vary=True,),
             amp=dict(value=np.max(adjusted_spectra[-1, :]) / np.max(interp_dists[len(_data[1:, 0])//2]), min=0),
             offset=dict(value=-np.min(interp_dists)),
             )
@@ -146,53 +178,68 @@ def main(filename):
 
     # for ind, spectrum in enumerate(adjusted_spectra):
     c = 0
-    rets = []
-    processes = []
-    q = mp.Queue()
-    for ind in range(0, len(adjusted_spectra[:, 0]), 40):
-        p = mp.Process(target=proc, args=(q, ind, adjusted_spectra, interp_dists, r, params))
-        processes.append(p)
-        p.start()
-        c += 1
-        print(c)
-    
-    for p in processes:
-        ret = q.get() # will block
-        rets.append(ret)
-    for p in processes:
-        p.join()
-
-    print(rets)
-
+    # num = len(adjusted_spectra[:, 0])
+    num = 8
+    start = time.perf_counter()
+    with mp.Pool(processes=8) as pool:
+        p = pool.map(partial(proc, dists=interp_dists, r=r, params=params), tqdm(adjusted_spectra[::ceil(len(adjusted_spectra[:, 0])/num), :]))
+    end = time.perf_counter()
+    print("Elapsed (after compilation) = {}s".format((end - start)))
     c = 0
+    # num = len(adjusted_spectra[:, 0])
+    num = 8
+    start = time.perf_counter()
+    p = []
+    for ind in tqdm(prange(0, len(adjusted_spectra[:, 0]), ceil( len(adjusted_spectra[:, 0]) / num))):
+        p.append(jitproc(ind, adjusted_spectra=adjusted_spectra, dists=interp_dists, r=r, params=params)) 
+    end = time.perf_counter()
+    print("Elapsed (after compilation) = {}s".format((end - start)))
+
+    dt = pybroom.tidy(p, var_names='time_pt')
+    dt.to_csv(P(filename).parent.joinpath(P(filename).stem + '_gaussian-fits.txt'), index=False)
+
+    for ind, res in enumerate(p):
     # for ind in list(out.keys()).sort():
-    #     ax.plot(specB, spectrum)
-    #     out = ret(res.params, interp_dists, r)
-    #     fwhm_ind = np.where(out > 0.5 * np.max(out))[0]
-    #     fwhm = np.abs(specB[fwhm_ind[0]] - specB[fwhm_ind[-1]])
-    #     ax.plot(specB, out + 1 + 0.1*c, label=f'ampgo {fwhm:.1f} G')
-    #     # out = ret(res2.params, interp_dists, r)
-    #     # fwhm_ind = np.where(out > 0.5 * np.max(out))[0]
-    #     # fwhm = np.abs(specB[fwhm_ind[0]] - specB[fwhm_ind[-1]])
-    #     # ax.plot(specB, out, label=meth + f' {fwhm:.1f} G')
+        spectrum = adjusted_spectra[ind, :]
+        ax.plot(specB, spectrum)
+        out = ret(res.params, interp_dists, r)
+        fwhm_ind = np.where(out > 0.5 * np.max(out))[0]
+        fwhm = np.abs(specB[fwhm_ind[0]] - specB[fwhm_ind[-1]])
+        ax.plot(specB, out + 1 + 0.1*c, label=f'ampgo {fwhm:.1f} G')
+        # out = ret(res2.params, interp_dists, r)
+        # fwhm_ind = np.where(out > 0.5 * np.max(out))[0]
+        # fwhm = np.abs(specB[fwhm_ind[0]] - specB[fwhm_ind[-1]])
+        # ax.plot(specB, out, label=meth + f' {fwhm:.1f} G')
 
-    #     a.plot(r, gaussian(r, res.params['r0'].value, res.params['w0'].value) + 10 * c, label='ampgo')
-    #     # a.plot(r, gaussian(r, res2.params['r0'].value, res2.params['w0'].value), label=meth)
+        # a.plot(r, gaussian(r, res.params['r0'].value, res.params['w0'].value) + 10 * c, label='ampgo')
+        a.plot(r, gaussian(r, res.params['r0'].value, res.params['w0'].value) + 10 * c, label=f"{res.params['r0'].value:.2f} nm")
+        a.annotate(f"{res.params['r0'].value:.2f} nm", (5, 1 + 10 * c ))
+        # a.plot(r, gaussian(r, res2.params['r0'].value, res2.params['w0'].value), label=meth)
 
-    #     # popt, _ = curve_fit(lorentzian, specB, spectrum)
-    #     # ax.plot(specB, lorentzian(specB, *popt), label='curve_fit')
+        # popt, _ = curve_fit(lorentzian, specB, spectrum)
+        # ax.plot(specB, lorentzian(specB, *popt), label='curve_fit')
 
-    #     aa.plot(specB, res.residual, label='ampgo')
-    #     # aa.plot(specB, res2.residual, label=meth)
-    #     # aa.plot(specB, (spectrum - lorentzian(specB, *popt))**2, label='curve_fit')
-
-    #     c += 1
+        aa.plot(specB, res.residual, label='ampgo')
+        # aa.plot(specB, res2.residual, label=meth)
+        # aa.plot(specB, (spectrum - lorentzian(specB, *popt))**2, label='curve_fit')
 
     #     # break
 
-    # ax.legend()
+    ax.set_ylabel('Signal')
+    ax.set_xlabel('Field (G)')
+    a.set_ylabel('Time')
+    a.set_xlabel('Distance (nm)')
+    aa.set_xlabel('Field')
+    aa.set_ylabel('Residual')
+
+    fg, ag = plt.subplots()
+    ag.plot([ii.params['r0'].value for ii in p])
+    ag.set_xlabel('Time')
+    ag.set_ylabel('Distance (nm)')
+
+    ax.legend()
     # a.legend()
-    # aa.legend()
+    aa.legend()
 
 if __name__ == "__main__":
     filename = '/Users/Brad/Library/CloudStorage/GoogleDrive-bdprice@ucsb.edu/My Drive/Research/Data/2023/5/30/FMN sample/stable/283.8/M01_283.8K_stable_pre30s_on10s_off470s_25000avgs_filtered_batchDecon.feather'
